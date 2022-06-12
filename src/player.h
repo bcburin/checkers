e12fda460b9f0783eea119a0c742490a838fe539
Bruno <cburin.bruno@gmail.com>
2022-06-12 19:44:16 -0300

    Implemented number of pieces for players

--- src/player.h
+++ src/player.h
@@ -13,6 +13,7 @@ namespace Checkers {
       std::string name_;
       Piece::ColorType piece_color_;
       Board& board_;
+      int num_pieces_;
     public:
       Player(std::string name, Piece::ColorType pc, Board& b)
       : name_(name), piece_color_(pc), board_(b) {}
@@ -20,6 +21,12 @@ namespace Checkers {
       std::string name() const { return name_; }
       
       Piece::ColorType piece_color() const { return piece_color_; }
+
+      int num_pieces() const { return num_pieces_; }
+
+      void add_piece(int n = 1) { num_pieces_ += n; }
+      
+      void remove_piece(int n = 1) { num_pieces_ = (n > num_pieces_) ? 0 : (num_pieces_ - n); }
       
       virtual typename Board::Move get_move() const = 0;
   };
