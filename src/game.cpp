a9e69590ec6a3931ca64846447a6b411eeb795fa
Bruno <cburin.bruno@gmail.com>
2022-06-13 10:37:23 -0300

    Implemented checkers game class

---
+++ src/game.cpp
@@ -0,0 +1,39 @@
+#include "game.h"
+
+Checkers::Game::Game(Type type, Piece::ColorType p1_color) {
+  Piece::ColorType p2_color = (p1_color == Piece::ColorType::dark) ? 
+                              Piece::ColorType::light :
+                              Piece::ColorType::dark;
+  switch(type) {
+    case Type::Human_Human:
+      player1 = new HumanPlayer("Player 1", p1_color, board_, std::cin);
+      player2 = new HumanPlayer("Player 2", p2_color, board_, std::cin);
+      break;
+    case Type::Human_AI:
+      // Not implemented yet
+      break;
+    case Type::AI_AI:
+      // Not implemented yet
+      break;
+  }
+
+  current = player1;
+}
+
+Checkers::Game::~Game() {
+  delete player1;
+  delete player2;
+}
+
+bool Checkers::Game::finished() {
+  return player1->num_pieces() == 0 || player2->num_pieces() == 0;
+}
+
+void Checkers::Game::play() {
+  Board::Move move = current->get_move();
+  // Validate piece colour
+  if(move.source().piece() && move.source().piece()->color() != current->piece_color())
+    throw std::invalid_argument("Invalid piece, you must move one of your own!"); 
+  // Perform movement
+  board_.move(move);
+}
---
+++ src/game.h
@@ -0,0 +1,33 @@
+#ifndef CHECKERS_GAME_H
+#define CHECKERS_GAME_H
+
+#include "board.h"
+#include "human_player.h"
+
+namespace Checkers {
+
+  class Game {
+    public:
+      enum class Type { Human_Human, Human_AI, AI_AI };
+    private:
+      Player* player1;
+      Player* player2;
+      Player* current;
+      Board board_;
+    public:
+      Game(Type type, Piece::ColorType p1_color = Piece::ColorType::dark);
+      
+      ~Game();
+      
+      bool finished();
+      
+      void play();
+
+      Board& board() { return board_; }
+      
+      void next_player() { current = (current == player1) ? player2 : player1; }
+  };
+
+};
+
+#endif
