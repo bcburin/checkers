8aa2eaf277d8ef2c0033e7014b5bd7416ffa7fb3
Bruno <cburin.bruno@gmail.com>
2022-06-15 12:19:41 -0300

    Colored input for human player moves

--- src/human_player.cpp
+++ src/human_player.cpp
@@ -1,8 +1,11 @@
 #include "human_player.h"
 
 typename Checkers::Board::Move Checkers::HumanPlayer::get_move() const {
+  set_color(Checker(piece_color_).color_w32());
   std::cout << std::endl << name() << " move: ";
+  set_color(WRITING_COLOR);
   std::string src, dest;
   is >> src >> dest;
+  reset_color();
   return Board::Move(board_.spot(src), board_.spot(dest), board_);
 }
